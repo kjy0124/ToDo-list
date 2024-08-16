e138c5306226ad3a380155bc433d0700614db435
kjy0124 <114080381+kjy0124@users.noreply.github.com>
2024-08-17 03:30:58 +0900

    240817(추가)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+
 const creatTodo = function () {
-  const todoList = document.querySelector('#todo-list');
   const newLi = document.createElement('li'); //createEleent를 사용하여 dom의 노드를 생성
   const newSpan = document.createElement('span');
   const newBtn = document.createElement('button');
-  console.log(newBtn);
 
   newBtn.addEventListener('click', () => {
     //버튼을 클릭했을 때
@@ -12,12 +12,17 @@ const creatTodo = function () {
     //toggle을 사용함으로써 처음에 버튼을 클릭하면 클래스가 생성되고 한 번 더 누르면 삭제
   });
 
+  newLi.addEventListener('dblclick', () => {
+    //버튼을 더블 클릭 했을 때 리스트 삭제
+    newLi.remove();
+  });
+
   newSpan.textContent = todoInput.value;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan); //newLi의 자식노드로 newSpan을 설정
   todoList.appendChild(newLi); //ul 태그 변수 todoList의 자식 노드로 newLi를 설정
   todoInput.value = ''; //input 창에 텍스트 입력 후 그 공간이 리스트에 입력되면 자동으로 빈 공간으로 만들어줌
-  console.log(newLi);
+  saveItemsFn();
 };
 
 const keyCodeCheck = function () {
@@ -25,4 +30,22 @@ const keyCodeCheck = function () {
   if (window.event.keyCode === 13 && todoInput.value !== '') {
     creatTodo();
   }
+};
+
+//전체 삭제를 위한 함수
+const deleteAll = function () {
+  const liList = document.querySelectorAll('li');
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+};
+
+const saveItemsFn = function () {
+  const savesItems = [];
+
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todo = todoList.children[i].querySelector('span').textContent;
+    savesItems.push(todo);
+  }
+  console.log(savesItems);
 };
