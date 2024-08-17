bfb3ed1370e6722d2ab902013c3d616c874104b8
kjy0124 <114080381+kjy0124@users.noreply.github.com>
2024-08-18 01:22:46 +0900

    240818(데이터 로컬스토리지 저장 기능 추가)

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const creatTodo = function () {
     //버튼을 클릭했을 때
     newLi.classList.toggle('complete'); //새로 생성된 newLi 태그에 complete 클래스를 추가
     //toggle을 사용함으로써 처음에 버튼을 클릭하면 클래스가 생성되고 한 번 더 누르면 삭제
+    saveItemsFn();
   });
 
   newLi.addEventListener('dblclick', () => {
@@ -40,12 +41,23 @@ const deleteAll = function () {
   }
 };
 
+//리스트 데이터 저장을 위한 함수
 const saveItemsFn = function () {
-  const savesItems = [];
+  const savesItems = []; //리스트 데이터를 담을 배열
 
+  //todoList의 자식요소 길이만큼 반복
   for (let i = 0; i < todoList.children.length; i++) {
-    const todo = todoList.children[i].querySelector('span').textContent;
-    savesItems.push(todo);
+    const todoObj = {
+      contents: todoList.children[i].querySelector('span').textContent, //todoList의 자식요소 개수만큼 반복하되 그 자식요소의 span의 textContent를 저장
+      complete: todoList.children[i].classList.contains('complete'),
+      /*todoList의 children의 i번째 li 태그가 가지고 있는 class 안에 
+      complete라고 하는 class가 존재 하는지 안하는지를 이 반복문에 담아줌
+      사용자가 버튼을 클릭했을 때 complete 값은 true 클릭하지 않았으면 false를 반환
+      */
+    };
+    savesItems.push(todoObj); //배열 안에 객체가 push
   }
-  console.log(savesItems);
+  //각 리스트를 저장하고 있는 배열을 로컬스토리지에 저장
+  //JSON : 객체나 배열 자체를 문자열로 변환해줄 수 있는 데이터 포멧
+  localStorage.setItem('saved-items', JSON.stringify(savesItems));
 };
